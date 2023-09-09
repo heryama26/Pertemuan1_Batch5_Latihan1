9cfe6af7e03654adaa9fcaaccf99609e9e2c5a2a
heryana.kasih <heryanakasih@gmail.com>
2023-09-09 11:04:56 +0700

    Latihan mandiri 3

---
+++ demo_dengan_titik/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <Latihan3/>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import Latihan3 from './components/Latihan3.vue'
+
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld,
+    Latihan3
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ demo_dengan_titik/src/components/Latihan3.vue
@@ -0,0 +1,46 @@
+<template>
+<div id="todo-list">
+    <form @submit.prevent="tambahkan">
+        <h1>My Todo List</h1>
+        <p v-for="text in listText" :key="text.text" @click="tambahkan">{{ text.text }}</p>
+        <input type="text" v-model='inputText'/> <button v-on:click="tambahkan">Tambahkan</button>
+        <p>{{ todoMore }}</p>
+    </form>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'Latihan-3',
+    data() {
+        return {
+        inputText: '',
+            listText: [{
+                text:''
+            }],
+            todoMore:''
+        }
+    },
+    methods:{
+        tambahkan(){
+            const trimmedText = this.inputText.trim()
+            if(trimmedText)
+            {
+                this.listText.push({
+                    text: trimmedText,
+                })
+                if(this.listText.length > 4)
+                {
+                    this.todoMore = 'Hebat!';
+                }
+                this.inputText = '';
+            }
+        }
+    }
+//     mounted() {
+//     const hello = 'Hello World!'
+//     debugger
+//     this.text = hello
+//   }
+}
+</script>
